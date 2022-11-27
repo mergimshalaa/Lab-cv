8cd3f9e06a7295b5a77c116000740e58e9eb029d
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2022-11-27 18:41:41 +0100

    toggle function darkmode & for pictures

---
+++ main.js
@@ -0,0 +1,31 @@
+window.addEventListener('DOMContentLoaded', main)
+
+function main() {
+    changeMode()
+    setupImageHover()
+}
+
+/*LocalStorage för toggle knappen*/
+function changeMode() {
+    const toggle = document.querySelector("#btn");
+    toggle.classList.toggle('active');
+    const element = document.body;
+    element.classList.toggle("night");
+}
+
+
+function setupImageHover() {
+    /** @type {HTMLImageElement} */
+    const img = document.querySelector(".picofme");
+    img.addEventListener('mouseenter', setNewPic);
+    img.addEventListener('mouseleave', setOldPic);
+}
+
+function setNewPic(event) {
+    event.target.src = "./img/IMG_5685.jpg";
+}
+
+function setOldPic(event) {
+    event.target.src = "./img/picofme.jpg";
+}
+
