2b459ba25b5065bb9de831b83ebd430f710cec33
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2022-12-01 00:54:16 +0100

    add toggle function on navbar

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 window.addEventListener('DOMContentLoaded', main)
-
+/**
+ * 
+ */
 function main() {
-    changeMode()
+    const menuItems = Array.from(document.querySelectorAll('.menu > li'))
+    menuItems.map(i => {
+        i.addEventListener('click', playSound)
+    })
     setupImageHover()
 }
 
-/*LocalStorage för toggle knappen*/
-function changeMode() {
-    const toggle = document.querySelector("#btn");
-    toggle.classList.toggle('active');
-    const element = document.body;
-    element.classList.toggle("night");
+/**
+ * 
+ */
+function playSound() {
+    document.querySelector('#sound').play();
 }
 
-
+/**
+ * 
+ */
 function setupImageHover() {
     /** @type {HTMLImageElement} */
     const img = document.querySelector(".picofme");
@@ -21,14 +27,38 @@ function setupImageHover() {
     img.addEventListener('mouseleave', setOldPic);
 }
 
+/**
+ * 
+ * @param {*} event 
+ */
 function setNewPic(event) {
     event.target.src = "./img/IMG_5685.jpg";
 }
 
+/**
+ * 
+ * @param {*} event 
+ */
 function setOldPic(event) {
     event.target.src = "./img/picofme.jpg";
 }
 
-function playSound() {
-    document.querySelector('#sound').play();
+/**
+ * 
+ */
+function changeMode() {
+    const toggle = document.querySelector("#btn-toggle");
+    toggle.classList.toggle('active');
+    const element = document.body;
+    element.classList.toggle("night");
 }
+
+/**
+ * 
+ */
+function toggleMenu() {
+    const menu = document.querySelector(".menu");
+    const close = document.querySelector(".fa-bars");
+    menu.classList.toggle('show')
+    close.classList.toggle('fa-times');
+} 
